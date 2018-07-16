aecee0aaecb33581da8417dca0cfb672fdec95a0
Suraj <suraj.bokey@gmail.com>
2018-07-16 21:40:58 +0530

    Read the different parts of the request and print on the console

--- index.js
+++ index.js
@@ -5,10 +5,36 @@
 
 // Dependencies
 var http = require('http');
+var url = require('url');
 
 // Server should respond to all the requests with a string
 var server = http.createServer(function(request, response){
+
+	// Get the url and parse it
+	var parsedUrl = url.parse(request.url, true);
+
+	// Get the path
+	var path = parsedUrl.pathname;
+	var trimmedPath = path.replace(/^\/+|\/+$/g,'');
+
+	// Get the query string parameter
+	var queryString = parsedUrl.query;
+
+	// Get the http method
+	var httpMethod = request.method;
+
+	// Get the header as an object
+	var headers = request.headers;
+
+	// Send the response
 	response.end('Hello World\n');
+
+	// Log the request
+	console.log('Path: ', path);
+	console.log('Trimmed path: ', trimmedPath);
+	console.log('Query string: ', queryString);
+	console.log('Http method: ', httpMethod);
+	console.log('Headers: ', headers);
 });
 
 // Start the server and have it listen on the port 3000
