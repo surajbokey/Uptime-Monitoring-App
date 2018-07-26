03c1728898e8c2993ed908a345c28eac491a07a5
Suraj <suraj.bokey@gmail.com>
2018-07-27 00:22:55 +0530

    Add hashing secrete in config file

--- config.js
+++ config.js
@@ -10,14 +10,16 @@ var environments = {};
 environments.staging = {
   'httpPort': 3000,
   'httpsPort': 3001,
-  'envName': 'staging'
+  'envName': 'staging',
+  'hashingSecret': 'someHashingSecret'
 };
 
 // Production environment
 environments.production = {
   'httpPort': 5000,
   'httpsPort': 5001,
-  'envName': 'production'
+  'envName': 'production',
+  'hashingSecret': 'productionHashingSecret'
 };
 
 // Determine which environment has been passed from command-line argument
