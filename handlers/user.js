6524540d8f625a439fa46c22b90b86f0f307d75a
Suraj <suraj.bokey@gmail.com>
2018-07-27 00:21:22 +0530

    Add the user handler to handle request releated to user's api

---
+++ handlers/user.js
@@ -0,0 +1,95 @@
+/*
+ * File to have all methods for user handler
+ *
+ */
+
+// Dependencies
+var _data = require('../lib/data');
+var helpers = require('../lib/helpers');
+
+var user = {};
+
+user.service = function(data, callback) {
+  var acceptableMethods = ['post', 'get', 'put', 'delete'];
+  if (acceptableMethods.indexOf(data.httpMethod) > -1) {
+    user[data.httpMethod](data, callback);
+  } else {
+    callback(405);
+  }
+};
+
+// Handle POST request
+// Required parms: firstName, lastName, phone, password, tosAgreement
+user.post = function(data, callback) {
+  // check for the required fields
+  var firstName = typeof(data.payload.firstName) == 'string' && data.payload.firstName.trim().length > 0 ? data.payload.firstName.trim() : false;
+  var lastName = typeof(data.payload.lastName) == 'string' && data.payload.lastName.trim().length > 0 ? data.payload.lastName.trim() : false;
+  var phone = typeof(data.payload.phone) == 'string' && data.payload.phone.trim().length == 10 ? data.payload.phone.trim() : false;
+  var password = typeof(data.payload.password) == 'string' && data.payload.password.trim().length > 0 ? data.payload.password.trim() : false;
+  var tosAgreement = typeof(data.payload.tosAgreement) == 'boolean' && data.payload.tosAgreement == true ? true : false;
+
+  if (firstName && lastName && phone && password && tosAgreement) {
+    // Check user does not exist
+    _data.read('users', phone, function(err, data) {
+      if (err) {
+        // Hash the password
+        var hashedPassword = helpers.hash(password);
+
+        if (hashedPassword) {
+          var userObject = {
+            'firstName': firstName,
+            'lastName': lastName,
+            'phone': phone,
+            'password': hashedPassword,
+            'tosAgreement': true
+          }
+
+          // Store the user
+          _data.create('users', phone, userObject, function(err) {
+            if (!err) {
+              callback(200);
+            } else {
+              console.log(err);
+              callback(500, {
+                'Error': 'Could not create a new user'
+              });
+            }
+          });
+        } else {
+          callback(500, {
+            'Error': 'Could not hash the user\'s password'
+          });
+        }
+      } else {
+        callback(400, {
+          'Error': 'User already exist'
+        })
+      }
+    });
+  } else {
+    callback(400, {
+      'Error': 'Missing required field'
+    });
+  }
+};
+
+// Handle GET request
+user.get = function(data, callback) {
+  console.log('Get request');
+  callback(200);
+};
+
+// Handle PUT request
+user.put = function(data, callback) {
+  console.log('Put request');
+  callback(200);
+};
+
+// Handle DELETE request
+user.delete = function(data, callback) {
+  console.log('Delete request');
+  callback(200);
+};
+
+// Export the module
+module.exports = user;
